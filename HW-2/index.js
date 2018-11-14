3c1d217ccb2f9569ca5137f99315c0393c91aa7d
griboedovvladimir <likecoffee@yandex.ru>
2018-11-15 00:16:28 +0300

    In progress

---
+++ HW-2/index.js
@@ -0,0 +1,9 @@
+const data = {
+  brand: {
+      Opel: ["Astra", "Vectra", "Zafira"],
+      Citroen: ["Xsara", "Xantia", "C5", "Berlingo"]
+  },
+    fuel:["petrol", "diesel"],
+    transmission: ["automatic", "manual"]
+};
+
