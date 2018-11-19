9f24b087efb58d1c3c079348c7f18e89a881b6a6
Vladimir <likecoffee@yandex.ru>
2018-11-19 19:59:41 +0300

    new commit

--- HW-3/scripts/index.js
+++ HW-3/scripts/index.js
@@ -2,32 +2,61 @@ const preloader = new Image(200, 200);
 preloader.src = 'preloader.svg';
 preloader.style.cssText = 'position: absolute; top: 50%; left: 50%; margin: -100px 0 0 -100px';
 
-function newsInit(id) {
+function newsInit(id = 'people') {
     document.body.appendChild(preloader);
-    renderNews(id);
+    renderNews(id).then(() => {
+        preloader.remove()
+    });
     navSwitcher(id);
 }
 
 async function renderNews(id) {
-// let response = await fetch('beckend.php',{
-//     method:'POST',
-//     headers: {
-//         "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
-//     },}).then(res=>res.json())
-    let response = await Promise.resolve('allOK');
-    dataPrepare(response);
+    let response = await fetch('beckend.php',{
+        method:'POST',
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+        },}).then(res=>res.json());
+    dataRender(dataParser(response));
 }
 
 function navSwitcher(id) {
     let navItems = document.getElementsByClassName('navigate-items');
-    for (el of navItems) {
+    for (let el of navItems) {
         el.classList.contains('active') ? el.classList.remove('active') : '';
         (el.id === id) ? el.classList.add('active') : '';
     }
 }
 
-function dataPrepare(response) {
-    console.log(response);
+function dataParser(response) {
+    let data = response.split(' class="news-tidings__item news-tidings__item_1of3 news-tidings__item_condensed "');
+    data.pop();
+    data.shift();
+    console.log(data);
+    let parsedData = data.reduce((cnt,el)=>{
+        let img = el.split(`<div class="news-tidings__image news-helpers_hide_mobile-small" style="background-image: url(`)[1].split(`);"></div>`)[0];
+        let link = 'https://realt.onliner.by' + el.split(`<a href="`)[1].split(`" class="news-tidings__stub"></a>`)[0];
+        let title = el.split(`<span class="news-helpers_hide_mobile-small">`)[1].split(`</span`)[0];
+        let discription = el.split(`<div class="news-tidings__speech news-helpers_hide_mobile-small">`)[1].split(`</div>`)[0].trim();
+        cnt.push({img, link, title, discription});
+        return cnt;
+    }, [])
+    console.log(parsedData);
+    return(parsedData)
+
+}
+
+function dataRender(data){
+    let news = data.reduce((cnt,el)=>{
+        let article = `${cnt}
+    <div class="news-wrapper">
+    <img class = "news-img" src = "${el.img}">
+    <a class = "news-title" href = "${el.link}">${el.title}</a>
+    <div class = "news-discription">${el.discription}</div>
+</div>
+    `;
+        return article;
+    } , '');
+    document.getElementById('newsWrapper').innerHTML = news;
 }
 
 document.getElementById('navigate').addEventListener('click', (e) => {
@@ -36,4 +65,4 @@ document.getElementById('navigate').addEventListener('click', (e) => {
     }
 });
 
-newsInit('people');
+newsInit();
