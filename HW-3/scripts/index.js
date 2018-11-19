60fce442f0ec16da3439a4febc52bb1cd84a9615
Vladimir <likecoffee@yandex.ru>
2018-11-19 13:55:06 +0300

    create structure

--- HW-3/scripts/index.js
+++ HW-3/scripts/index.js
@@ -1,6 +1,25 @@
 
+
+
 console.log('Working');
 fetch('beckend.php',{method:'POST',
     headers: {
         "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
     },}).then(res=> {res.text().then(result => console.log(JSON.parse(result).split(' <div class="news-tidings__item news-tidings__item_1of3 news-tidings__item_condensed "')))}) ;
+
+document.getElementById('navigate').addEventListener('click', (e)=>{
+           renderNews(e.target.id);
+           navSwitcher(e.target.id);
+});
+
+function renderNews(id){
+
+}
+
+function navSwitcher(id){
+
+}
+
+function dataPrepere(){
+
+}
