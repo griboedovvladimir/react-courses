fd4ff03a57a8347834d7bd8ef0b08baaa2e1b4d8
griboedovvladimir <likecoffee@yandex.ru>
2018-12-17 01:18:55 +0300

    Commit

---
+++ hw-7/build/precache-manifest.d459a0480e1ac6afd7d49128c20bafb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a643fd0809c93a94591",
+    "url": "/static/js/main.8a643fd0.chunk.js"
+  },
+  {
+    "revision": "be7b1f34f2da9bc1f117",
+    "url": "/static/js/1.be7b1f34.chunk.js"
+  },
+  {
+    "revision": "8a643fd0809c93a94591",
+    "url": "/static/css/main.ca45c608.chunk.css"
+  },
+  {
+    "revision": "a79eaef31379c6e12f262a542c57ba8f",
+    "url": "/index.html"
+  }
+];
