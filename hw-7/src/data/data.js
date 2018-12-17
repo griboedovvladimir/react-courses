f5db44bdadffbc84107e43962535de94e03db2e3
griboedovvladimir <likecoffee@yandex.ru>
2018-12-17 23:00:02 +0300

    Updates

--- hw-7/src/data/data.js
+++
@@ -1,9 +0,0 @@
-export const data = {
-    brand: {
-        Opel: ["Astra", "Vectra", "Zafira"],
-        Citroen: ["Xsara", "Xantia", "C5", "Berlingo"],
-        Ford: ["Focus","Fiesta"]
-    },
-    fuel:["Petrol", "Diesel"],
-    transmission: ["Automatic", "Manual"]
-};
---
+++ precache-manifest.ce7c5e25f8cb38f567a8d4115ddbf33a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82ef02a739b7d2f5ed2f",
+    "url": "/react-courses/hw-7/static/js/runtime~main.82ef02a7.js"
+  },
+  {
+    "revision": "64fa28d8f359243d5b05",
+    "url": "/react-courses/hw-7/static/js/main.64fa28d8.chunk.js"
+  },
+  {
+    "revision": "be7b1f34f2da9bc1f117",
+    "url": "/react-courses/hw-7/static/js/1.be7b1f34.chunk.js"
+  },
+  {
+    "revision": "64fa28d8f359243d5b05",
+    "url": "/react-courses/hw-7/static/css/main.ca45c608.chunk.css"
+  },
+  {
+    "revision": "eb06a7789d91600373243b73dd40e7ce",
+    "url": "/react-courses/hw-7/index.html"
+  }
+];
