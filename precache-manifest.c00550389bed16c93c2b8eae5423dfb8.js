a5d9c6dc16d08be79c9186f4b0762b6b909ed7d3
griboedovvladimir <likecoffee@yandex.ru>
2019-01-07 13:57:01 +0300

    Updates

---
+++ precache-manifest.c00550389bed16c93c2b8eae5423dfb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24f7960ffb3d26c07b11",
+    "url": "/react-courses/static/js/runtime~main.24f7960f.js"
+  },
+  {
+    "revision": "2aff9395eb50ade8f9a5",
+    "url": "/react-courses/static/js/main.2aff9395.chunk.js"
+  },
+  {
+    "revision": "5cb9b55e9859431b45ee",
+    "url": "/react-courses/static/js/1.5cb9b55e.chunk.js"
+  },
+  {
+    "revision": "2aff9395eb50ade8f9a5",
+    "url": "/react-courses/static/css/main.12cc9c96.chunk.css"
+  },
+  {
+    "revision": "68ae41e817152291af6d2ae7b620478b",
+    "url": "/react-courses/index.html"
+  }
+];
