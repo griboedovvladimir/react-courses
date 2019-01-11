60a0932643f2ee3b54bc24cd9336df2f6a65eacc
griboedovvladimir <likecoffee@yandex.ru>
2019-01-11 08:36:44 +0300

    commit

--- hw-10-redux-calculator/src/reducers/reducers.ts
+++ hw-10-redux-calculator/src/reducers/reducers.ts
@@ -20,38 +20,32 @@ interface IState {
 }
 
 export function calculator(state: IState = InitialState, action: IActions): IState {
-    let newState = state;
     switch (action.type) {
         case CONSTANTS.ADD_CHAR:
-            newState = {
+             return {
                 ...state, currentNumberString: state.currentNumberString + action.char
             };
-            break;
         case CONSTANTS.REMEMBER_NUMBER_AND_OPERATION:
-            newState = {
+            return {
                 ...state, memory: action.number, operation: action.operation
             };
-            break;
         case CONSTANTS.CLEAR_CURRENT_NUMBER:
-            newState = {
+            return {
                 ...state, currentNumberString: ''
             };
-            break;
         case CONSTANTS.CLEAR:
-            newState = {
+            return {
                 currentNumberString: '',
                 memory: 0,
                 operation: '',
             };
-            break;
         case CONSTANTS.CLEAR_MEMORY:
-            newState = {
+            return {
                 currentNumberString: '0',
                 memory: 0,
                 operation: '',
             };
-            break;
+        default:
+            return state;
     }
-
-    return newState;
 }
