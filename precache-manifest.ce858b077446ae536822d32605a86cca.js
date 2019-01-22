56facca304ae9ce3217728233fef99771853962a
griboedovvladimir <likecoffee@yandex.ru>
2019-01-23 00:40:29 +0300

    Updates

---
+++ precache-manifest.ce858b077446ae536822d32605a86cca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24f7960ffb3d26c07b11",
+    "url": "/react-courses/static/js/runtime~main.24f7960f.js"
+  },
+  {
+    "revision": "ae1382cc69a1b6ff7127",
+    "url": "/react-courses/static/js/main.ae1382cc.chunk.js"
+  },
+  {
+    "revision": "c72f0a6cff569950c620",
+    "url": "/react-courses/static/js/1.c72f0a6c.chunk.js"
+  },
+  {
+    "revision": "ae1382cc69a1b6ff7127",
+    "url": "/react-courses/static/css/main.c72f369a.chunk.css"
+  },
+  {
+    "revision": "5c8cda32ddc0a1753e3673eccc462717",
+    "url": "/react-courses/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-courses/precache-manifest.6b3eea0b16aab5b9d4181c9b837e63fc.js"
+  "/react-courses/precache-manifest.ce858b077446ae536822d32605a86cca.js"
 );
 
 workbox.clientsClaim();
